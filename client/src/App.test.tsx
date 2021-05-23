fd63e364fd46b2a47db87bd7fb71ec1aeb611d2f
ruwani <ruwani.elapatha@attuneconsulting.com>
2021-05-23 11:25:33 +0530

    added identified test cases

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -7,3 +7,25 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('display all photoes that user selected in gphotogid view', () => {
+
+});
+
+test('display order in the best photo gallery is correct as user selected', () => {
+
+});
+
+test('not allowing to select more than 9 photoes', () => {
+
+});
+
+test('photo order is correctly updating with regard to user click on photo', () => {
+
+});
+
+test('photo order is correctly updateing when user deselect a photo', () => {
+
+});
+
+
